91f327b9bb9b0f1a9e705d1e96f8ad94cee95293
student-yashsingh <yash1243singh@gmail.com>
2025-05-09 21:18:17 +0530

    Initial commit with backend

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require('sillyname');
+// import generateName from 'sillyName';
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am the superhero and my name is ${name} `)
